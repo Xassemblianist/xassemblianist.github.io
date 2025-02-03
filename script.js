de4a2cf618588c98ade1a60622241d9ebb1b51b3
Xassemblianist <106768809+Xassemblianist@users.noreply.github.com>
2025-02-03 19:42:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 alert("Site daha alpha aşamasındadır.");
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '10px';
+  }
+}
